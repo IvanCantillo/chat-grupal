805883e704694dcfb4dd14579d3299e215a8c366
IvanCantillo <49213723+IvanCantillo@users.noreply.github.com>
2020-10-27 17:08:29 -0500

    Nueva estructura

--- public/js/notify.js
+++ public/js/notify.js
@@ -1,32 +1,28 @@
 const notifySuccess = (data) => {
-    let html = `
-        <div class="col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-success text-white rounded py-2 notify-show">
-            ¡<strong>${data}</strong> ha ingresado! 🥳
-        </div>
-    `;
+    let html = document.createElement("div");
+    html.setAttribute("id", "notify-success");
+    html.setAttribute("class", "col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-success text-white rounded py-2 notify-show");
+    html.innerHTML = `¡<strong>${data}</strong> ha ingresado! 🥳`;
     return html;
 }
 const notifyDanger = (data) => {
-    let html = `
-        <div class="col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-danger text-white rounded py-2 notify-show">
-            ¡<strong>${data}</strong> se ha salido! 😭
-        </div>
-    `;
+    let html = document.createElement("div");
+    html.setAttribute("id", "notify-danger");
+    html.setAttribute("class", "col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-danger text-white rounded py-2 notify-show");
+    html.innerHTML = `¡<strong>${data}</strong> se ha salido! 😭`;
     return html;
 }
 const notifyInfo = (data) => {
-    let html = `
-        <div class="col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-info text-white rounded py-2 notify-show">
-            ¡<strong>${data}</strong> info! 👌
-        </div>
-    `;
+    let html = document.createElement("div");
+    html.setAttribute("id", "notify-info");
+    html.setAttribute("class", "col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-info text-white rounded py-2 notify-show");
+    html.innerHTML = `¡<strong>${data}</strong> info! 👌`;
     return html;
 }
 const notifyWarning = (data) => {
-    let html = `
-        <div class="col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-warning text-white rounded py-2 notify-show">
-            ¡<strong>${data}</strong> warning! 😱
-        </div>
-    `;
+    let html = document.createElement("div");
+    html.setAttribute("id", "notify-warning");
+    html.setAttribute("class", "col-6 col-md-4 col-lg-3 col-xl-3 ml-auto my-1 bg-warning text-white rounded py-2 notify-show");
+    html.innerHTML = `¡<strong>${data}</strong> warning! 😱`;
     return html;
 }
